56a4636be3e13d9aaa2ba713d95f6dbc66d21bb7
Cedric Chee <cedric@invictusbyte.com>
2020-03-28 18:43:48 +0800

    2.9 REPL (read-parse-print-loop)

--- repl/repl.go
+++ repl/repl.go
@@ -9,11 +9,26 @@ import (
 	"io"
 
 	"github.com/cedrickchee/hou/lexer"
-	"github.com/cedrickchee/hou/token"
+	"github.com/cedrickchee/hou/parser"
 )
 
 // PROMPT is the REPL prompt displayed for each input.
 const PROMPT = ">> "
+
+// MONKEYFACE is the REPL's face if we run into any parser errors. You get to
+// see a monkey :D
+const MONKEYFACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
 
 // Start starts the REPL in a continuous loop.
 func Start(in io.Reader, out io.Writer) {
@@ -28,10 +43,29 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 
-		// A REPL that tokenizes Monkey source code and prints the tokens.
+		// A REPL that tokenizes and parses Monkey source code and prints
+		// the AST.
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+
+		// Print stringified version of the AST to stdout.
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+// Print parser errors to stdout.
+func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEYFACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, "parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
